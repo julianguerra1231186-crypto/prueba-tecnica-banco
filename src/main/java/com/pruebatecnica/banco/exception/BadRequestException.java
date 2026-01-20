87094bfd3e84d1800014f67976f1fd9590d9729f
julianguerra1231186-crypto <julianguerra1231186@gmail.com>
2026-01-19 19:18:29 -0500

    docs: agregar documentación JavaDoc en excepciones

--- src/main/java/com/pruebatecnica/banco/exception/BadRequestException.java
+++ src/main/java/com/pruebatecnica/banco/exception/BadRequestException.java
@@ -3,6 +3,11 @@ package com.pruebatecnica.banco.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Excepción lanzada cuando se presenta una solicitud inválida
+ * durante la ejecución de una operación.
+ */
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
 
