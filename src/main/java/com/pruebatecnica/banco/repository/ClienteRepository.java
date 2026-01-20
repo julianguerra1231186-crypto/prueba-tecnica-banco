b1da9b1eea4f1d8215b4b9fd115155b781e9b5c3
julianguerra1231186-crypto <julianguerra1231186@gmail.com>
2026-01-19 19:13:26 -0500

    docs: agregar documentación JavaDoc en repositorios

--- src/main/java/com/pruebatecnica/banco/repository/ClienteRepository.java
+++ src/main/java/com/pruebatecnica/banco/repository/ClienteRepository.java
@@ -2,6 +2,10 @@ package com.pruebatecnica.banco.repository;
 
 import com.pruebatecnica.banco.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+/**
+ * Repositorio para la gestión de la entidad Cliente.
+ * Proporciona acceso a la persistencia de datos.
+ */
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
